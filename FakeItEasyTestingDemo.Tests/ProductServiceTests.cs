24b69e5f7427b6e8b460d218e55c5ce956d571ae
Brian Behm <brian@behm.net>
2017-10-06 14:07:11 -0500

    Added GetProduct tests

--- FakeItEasyTestingDemo.Tests/ProductServiceTests.cs
+++ FakeItEasyTestingDemo.Tests/ProductServiceTests.cs
@@ -68,5 +68,63 @@ namespace FakeItEasyTestingDemo.Tests
 			Assert.IsTrue(results.Count(p => p.InStock == true) == 3);
 			Assert.IsTrue(results.Count(p => p.InStock == false) == 0);
 		}
+
+		[TestMethod]
+		public void GetProduct_ValidId_ReturnsProduct()
+		{
+			//Arrange
+			var data = new List<Product>
+			{
+				new Product { Id = 1, Name = "Widget", InStock = true },
+				new Product { Id = 2, Name = "Gadget", InStock = true },
+				new Product { Id = 3, Name = "Thingy", InStock = false },
+				new Product { Id = 4, Name = "Whirlyjig", InStock = true },
+			};
+
+			var set = A.Fake<DbSet<Product>>(o => o.Implements(typeof(IQueryable<Product>)).Implements(typeof(IDbAsyncEnumerable<Product>))).SetupData(data);
+
+			var productId = 1;
+
+			var context = A.Fake<AcmeContext>();
+			A.CallTo(() => context.Products).Returns(set);
+			//A.CallTo(() => context.Products.Find(A<int>.Ignored)).ReturnsLazily(objectCall => context.Products.FirstOrDefault(t => t.Id == objectCall.GetArgument<int>(0)));
+			A.CallTo(() => context.Products.Find(productId)).Returns(context.Products.FirstOrDefault(t => t.Id == productId));
+
+			IProductService service = new ProductService(context);
+
+			//Act 
+			var result = service.GetProduct(productId);
+
+			//Assert
+			Assert.IsNotNull(result);
+			Assert.IsTrue(result.Id == productId);
+		}
+
+		[TestMethod]
+		public void GetProduct_InvalidId_ReturnsNull()
+		{
+			//Arrange
+			var data = new List<Product>
+			{
+				new Product { Id = 1, Name = "Widget", InStock = true },
+				new Product { Id = 2, Name = "Gadget", InStock = true },
+				new Product { Id = 3, Name = "Thingy", InStock = false },
+				new Product { Id = 4, Name = "Whirlyjig", InStock = true },
+			};
+
+			var set = A.Fake<DbSet<Product>>(o => o.Implements(typeof(IQueryable<Product>)).Implements(typeof(IDbAsyncEnumerable<Product>))).SetupData(data);
+
+			var context = A.Fake<AcmeContext>();
+			A.CallTo(() => context.Products).Returns(set);
+
+			IProductService service = new ProductService(context);
+
+			//Act 
+			var productId = 99;
+			var result = service.GetProduct(productId);
+
+			//Assert
+			Assert.IsNull(result);
+		}
 	}
 }
